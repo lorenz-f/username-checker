3cb3aef0640599631b39f7aae44605fed46d740a
Lorenzo <67831959+lorenz-f@users.noreply.github.com>
2023-06-12 18:44:29 -0400

    added screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        'sm': {'max': '700px'}
+      }
     },
   },
   plugins: [],
